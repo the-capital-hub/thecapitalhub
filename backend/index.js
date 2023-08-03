61e2945eedf06576aded6c794b55be31c8505476
Rajupzenrays <raazuuprasain94@gmail.com>
2023-08-03 12:27:46 +0530

    added the origin uri

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cors(options));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://thecapitalhubbackend.onrender.com"],
     methods: ["POST", "GET", "PUT"],
     credentials: true,
   })
