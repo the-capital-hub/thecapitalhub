0946a9825e87cbec91397d71b7988d8a0bd48e55
Rajupzenrays <raazuuprasain94@gmail.com>
2023-08-03 13:03:51 +0530

    added updated uri

--- backend/constants/db.js
+++ backend/constants/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://raazuuprasain94:TheCapitalHub@123@cluster0.3qsyv8d.mongodb.net/?retryWrites=true&w=majority&dbname=thecapitalhub";
+const uri = "mongodb+srv://raazuuprasain94:TheCapitalHub@123@cluster0.3qsyv8d.mongodb.net/thecapitalhub";
 const client = new MongoClient(uri);
 
 async function connectToMongoDB() {
