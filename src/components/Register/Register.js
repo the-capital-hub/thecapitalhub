1178dc987af75e466c84af00c599fa05721f22ad
the-capital-hub <141011134+the-capital-hub@users.noreply.github.com>
2023-08-17 13:59:12 +0530

    Home and Posts responsiveness.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -415,7 +415,8 @@ const Register = () => {
             <br />
           </div>
         )}
-        {showStartUp && <StartUpForm />}
+        {/* {showStartUp && <StartUpForm />} */}
+        {!showStartUp && <StartUpForm />}
         {showInvestor && <InvestorForm />}
         {showSelectWhatYouAre && (
           <SelectWhatYouAre
