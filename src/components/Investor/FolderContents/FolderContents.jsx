263021190db0d65ab6ea1b9b8bf2bd78f691a389
= <=>
2023-08-23 13:20:26 +0530

    documentation page UI changes

--- src/components/Investor/FolderContents/FolderContents.jsx
+++ src/components/Investor/FolderContents/FolderContents.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import "./FolderContents.scss";
 import { useParams } from "react-router-dom";
+import HalfbendCard from "../InvestorGlobalCards/Documentation/HalfbendCard/HalfbendCard";
 
 const FolderContents = () => {
+  const [files, setFiles] = useState(["file1", "file2"]);
   const { route } = useParams();
 
-  return <div>{route}</div>;
+  const title = route[0].toUpperCase() + route.slice(1);
+  return (
+    <div className="folderContents">
+      <h1>{title}</h1>
+      <HalfbendCard />
+    </div>
+  );
 };
 
 export default FolderContents;
