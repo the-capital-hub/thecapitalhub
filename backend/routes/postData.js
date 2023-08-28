c94e6b116b59fe82794a622d2eb5b4df1865df2c
Adithya <adithyahebbar32@gmail.com>
2023-08-28 17:52:05 +0530

    added routes to getting saved post

--- backend/routes/postData.js
+++ backend/routes/postData.js
@@ -4,14 +4,18 @@ import {
   getAllPosts,
   getSinglePost,
   savePostController,
+  getAllSavedPostCollectionsController,
+  getSavedPostsByCollectionController,
   likeUnlikePostController,
   commentOnPostController,
   getCommentsController,
+  getLikeCountController,
+  getUsersWhoLikedPostController,
 } from "../controllers/postController.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 const router = express.Router();
 
-router.use(authenticateToken);
+// router.use(authenticateToken);
 
 router.get("/getposts", getAllPosts);
 router.get("/getSinglePost/:id", getSinglePost);
@@ -20,9 +24,12 @@ router.get("/getSinglePost/:id", getSinglePost);
 router.post("/newPost", createPost);
 // router.patch("/oldsavePost/:postId", savePost);
 router.patch("/savePost/:postId", savePostController);
-// router.get("/savedPosts", getSavedPosts);
+router.get("/getSavedPostCollections/:userId", getAllSavedPostCollectionsController);
+router.get("/getSavedPostsByCollection/:userId", getSavedPostsByCollectionController);
 
 router.post("/likeUnlikePost/:postId", likeUnlikePostController);
+router.get('/likeCount/:postId', getLikeCountController);
+router.get('/likedUsers/:postId', getUsersWhoLikedPostController);
 router.post("/comment/:postId", commentOnPostController);
 router.get("/getComments/:postId", getCommentsController);
 
