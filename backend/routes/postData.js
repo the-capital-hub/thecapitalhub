8b8011e6c5f322715f78f0300456a5acd5179a65
Adithya <adithyahebbar32@gmail.com>
2023-08-28 17:59:30 +0530

    uncommented middleware

--- backend/routes/postData.js
+++ backend/routes/postData.js
@@ -15,10 +15,10 @@ import {
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 const router = express.Router();
 
-// router.use(authenticateToken);
+router.use(authenticateToken);
 
 router.get("/getposts", getAllPosts);
-router.get("/getSinglePost/:id", getSinglePost);
+router.get("/getSinglePost/:id", getSinglePost);  
 
 // Single user routes
 router.post("/newPost", createPost);
