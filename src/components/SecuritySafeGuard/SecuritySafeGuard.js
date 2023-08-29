0c43803696dceebd1eb3daf7369f800222ead691
the-capital-hub <learn.capitalhub@gmail.com>
2023-08-29 14:02:52 +0530

    added three pages privacy policy

---
+++ src/components/SecuritySafeGuard/SecuritySafeGuard.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./SecuritySafeGuard.scss";
+
+const SecuritySafeGuard = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="container ssg_container">
+          <section className="title_section">
+            <h1 className="term_title">Security Safeguards</h1>
+          </section>
+          <section className="title__section">
+            <h2 className="heading_with_line_one">Security</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Urna at dolor orci donec.
+              Enim fermentum tempus ipsum porttitor vehicula vel. Dui nibh
+              egestas diam diam ipsum elementum viverra. Non quis eros mauris
+              gravida lacus quis. Varius tempus commodo purus et.Lorem ipsum
+              dolor sit amet consectetur. Urna at dolor orci donec. Enim
+              fermentum tempus ipsum porttitor vehicula vel. Dui nibh egestas
+              diam diam ipsum elementum viverra. Non quis eros mauris gravida
+              lacus quis. Varius tempus commodo purus et.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Urna at dolor orci donec.
+              Enim fermentum tempus ipsum porttitor vehicula vel. Dui nibh
+              egestas diam diam ipsum elementum viverra. Non quis eros mauris
+              gravida lacus quis. Varius tempus commodo purus et.Lorem ipsum
+              dolor sit amet consectetur. Urna at dolor orci donec. Enim
+              fermentum tempus ipsum porttitor vehicula vel. Dui nibh egestas
+              diam diam ipsum elementum viverra. Non quis eros mauris gravida
+              lacus quis. Varius tempus commodo purus et.
+            </p>
+          </section>
+
+          <section className="title__section">
+            <h2 className="heading_with_line_two">Service Providers</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Urna at dolor orci donec.
+              Enim fermentum tempus ipsum porttitor vehicula vel. Dui nibh
+              egestas diam diam ipsum elementum viverra. Non quis eros mauris
+              gravida lacus quis. Varius tempus commodo purus et.Lorem ipsum
+              dolor sit amet consectetur. Urna at dolor orci donec. Enim
+              fermentum tempus ipsum porttitor vehicula vel. Dui nibh egestas
+              diam diam ipsum elementum viverra. Non quis eros mauris gravida
+              lacus quis. Varius tempus commodo purus et.
+            </p>
+          </section>
+
+          <section className="title__section">
+            <h2 className="heading_with_line_three">Analytics</h2>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Urna at dolor orci donec.
+              Enim fermentum tempus ipsum porttitor vehicula vel. Dui nibh
+              egestas diam diam ipsum elementum viverra. Non quis eros mauris
+              gravida lacus quis. Varius tempus commodo purus et.Lorem ipsum
+              dolor sit amet consectetur. Urna at dolor orci donec. Enim
+              fermentum tempus ipsum porttitor vehicula vel. Dui nibh egestas
+              diam diam ipsum elementum viverra. Non quis eros mauris gravida
+              lacus quis. Varius tempus commodo purus et.
+            </p>
+          </section>
+
+
+          <section className="title__section">
+            <h2 className="heading_with_line_four">Security of Data</h2>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Urna at dolor orci donec.
+              Enim fermentum tempus ipsum porttitor vehicula vel. Dui nibh
+              egestas diam diam ipsum elementum viverra. Non quis eros mauris
+              gravida lacus quis. Varius tempus commodo purus et.Lorem ipsum
+              dolor sit amet consectetur. Urna at dolor orci donec. Enim
+              fermentum tempus ipsum porttitor vehicula vel. Dui nibh egestas
+              diam diam ipsum elementum viverra. Non quis eros mauris gravida
+              lacus quis. Varius tempus commodo purus et.
+            </p>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SecuritySafeGuard;
