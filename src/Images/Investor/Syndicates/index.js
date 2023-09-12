b7a3164df07c7518cf0284f4f9c073acb03c0e11
srihari-m-s <srihari.magadi@gmail.com>
2023-09-12 19:21:21 +0530

    Add components and styles for Syndicates Page

---
+++ src/Images/Investor/Syndicates/index.js
@@ -0,0 +1,21 @@
+import Location from "./Group.svg";
+import ThreeUser from "./three-user.svg";
+import WebIcon from "./website.svg";
+import UserOne from "./user-1.png";
+import UserTwo from "./user-2.png";
+import HCL from "./HCL.png";
+import TCS from "./TCS.png";
+import Infosys from "./Infosys.png";
+import Wipro from "./Wipro.png";
+
+export {
+  Location,
+  ThreeUser,
+  WebIcon,
+  UserOne,
+  UserTwo,
+  HCL,
+  TCS,
+  Infosys,
+  Wipro,
+};
--- src/pages/Investor/Syndicates/Syndicates.jsx
+++ src/pages/Investor/Syndicates/Syndicates.jsx
@@ -1,12 +1,31 @@
 import SmallProfileCard from "../../../components/Investor/InvestorGlobalCards/TwoSmallMyProfile/SmallProfileCard";
+import CompanyCardContainer from "../../../components/Investor/SyndicateComponents/CompanyCardContainer";
 import "./Syndicates.scss";
 
 export default function Syndicates() {
   return (
     <div className="syndicates__container">
       <SmallProfileCard text="Syndicates" />
-      <section className="syndicates__section">
-        <h1>companies</h1>
+      <section className="syndicates__section__wrapper container-xxl ">
+        <div className="companies__container border-bottom ">
+          <div className="companies__header py-3 px-4 d-flex justify-content-between align-items-center border-bottom">
+            <h2 className="main__heading div__heading">Companies</h2>
+            <div className="main__btn">
+              {/* <Button text="Create a Group" /> */}
+              <button className="btn-capital">Create a Group</button>
+            </div>
+          </div>
+          <div className="container__padding py-5">
+            <CompanyCardContainer key={"companies"} isCompanies />
+          </div>
+        </div>
+
+        <div className="membership__container mt-4 py-1 px-4 d-flex justify-content-between align-items-center border-bottom">
+          <h2 className="main__heading div__heading mb-2">Membership</h2>
+        </div>
+        <div className="container__padding py-5">
+          <CompanyCardContainer key={"members"} text={"Membership"} />
+        </div>
       </section>
     </div>
   );
