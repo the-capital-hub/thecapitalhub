def19d7e1829e5114da024a3f9fc31181df1d28e
srihari-m-s <srihari.magadi@gmail.com>
2023-09-16 18:55:15 +0530

    fix: border and padding on Calendar

--- src/pages/Investor/MySchedule/MySchedule.jsx
+++ src/pages/Investor/MySchedule/MySchedule.jsx
@@ -18,17 +18,17 @@ export default function MySchedule() {
       <div className="pb-4 pt-2">
         <SmallProfileCard text="My Schedule" />
       </div>
-      <section className="section__wrapper bg-white rounded-4 border mb-5">
+      <section className="section__wrapper bg-white rounded-3 border mb-5 pb-5 d-flex flex-column justify-content-center gap-5">
         <div className="d-flex justify-content-between align-items-center border-bottom p-3">
           <ViewSelect handleViewSelect={handleViewSelect} />
           <button className="btn-capital">Create Meeting</button>
         </div>
 
-        <div className="schedule__container d-flex">
-          <div className="w-75 p-3 border-end">
+        <div className="schedule__container px-3 d-flex justify-content-between gap-5">
+          <div className="calender__div w-75">
             <CalendarContainer view={view} />
           </div>
-          <div className="w-25 p-3">
+          <div className="meetings__div w-25 p-3 border rounded-4">
             <h1>My Meetings</h1>
           </div>
         </div>
