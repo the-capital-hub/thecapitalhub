a324cb3ef37669b5038b2f5161e0e92ffa1ee9fc
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-09-19 14:14:16 +0530

    chat page back button is link with profile

--- src/pages/ChatPages/Chats/ChatSearch/ChatSearch.js
+++ src/pages/ChatPages/Chats/ChatSearch/ChatSearch.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatSearch.scss";
 import backIcon from "../../../../Images/Chat/BackIcon.svg";
+import { Link } from "react-router-dom";
+import { getUserChats } from "../../../../Service/user";
+import { useSelector } from "react-redux";
+
+
 
 const ChatSearch = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const loggedInUser = useSelector((state) => state.user.loggedInUser);
+
+
+  const handleSearch = (query) => {
+      getUserChats(loggedInUser._id)
+        .then((res) => {
+          console.log(res.data);
+          setSearchQuery(query);
+    // const filtered = data.filter(item =>
+    //   item.name.toLowerCase().includes(query.toLowerCase())
+    // );
+    // setFilteredData(filtered);
+        })
+        .catch((error) => {
+          console.error("Error-->", error);
+        });
+    
+  };
+
+  const handleInputChange = (event) => {
+    const query = event.target.value;
+    handleSearch(query);
+  };
   return (
     <>
       <div className="chat_search_container">
         <span>
-          <img src={backIcon} className="back_img" />
+        <Link
+        to={`/profile`}>
+        <img src={backIcon} className="back_img" />
+        </Link>
         </span>
         <h1 className="chat_title">Chats</h1>
         <div className="inputs">
-          <input type="search" className="search_chat" />
+          <input type="search" className="search_chat"  placeholder="Search..." value={searchQuery}  onChange={handleInputChange} />
+          <div className="recommendations">
+        {filteredData.length > 0 && (
+          <ul>
+            {filteredData.map((item, index) => (
+              <li key={index}>{item.name}</li>
+            ))}
+          </ul>
+        )}
+        {filteredData.length === 0 && searchQuery && (
+          <p>No results found.</p>
+        )}
+      </div>
         </div>
       </div>
     </>
--- src/pages/ChatPages/Chats/Chats.js
+++ src/pages/ChatPages/Chats/Chats.js
@@ -21,7 +21,12 @@ const Chats = () => {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [sendMessage, setSendMessage] = useState(null);
   const [recieveMessage, setRecieveMessage] = useState(null);
+  // const previousUrl = window.history.length > 1 ? window.history.go(-1) : null;
 
+  // if (previousUrl) {
+  //   console.log('Previous URL:', window.location.href);
+  // }
+  
   const socket = useRef();
 
   const disconnectSocket = () => {
@@ -90,7 +95,7 @@ const Chats = () => {
     <>
       <div className="container-fluid chat_main_container">
         <section className="left_section">
-          <ChatSearch />
+          <ChatSearch  />
           <ChatSidebar
             selectedChat={setSelectedChat}
             setSelectedUser={setSelectedUser}
