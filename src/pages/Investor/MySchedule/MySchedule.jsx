42f565d70bf6252b11b4229a791341a26c2d5fe8
srihari-m-s <srihari.magadi@gmail.com>
2023-09-19 14:37:11 +0530

    my schedule page responisveness

--- src/pages/Investor/MySchedule/MySchedule.jsx
+++ src/pages/Investor/MySchedule/MySchedule.jsx
@@ -18,18 +18,25 @@ export default function MySchedule() {
       <div className="pb-4 pt-2">
         <SmallProfileCard text="My Schedule" />
       </div>
-      <section className="section__wrapper bg-white rounded-3 border mb-5 pb-5 d-flex flex-column justify-content-center gap-5">
-        <div className="d-flex justify-content-between align-items-center border-bottom p-3">
+      <section className="section__wrapper bg-white rounded-3 border mb-5 pb-5 d-flex flex-column gap-5">
+        <div className="d-flex flex-column flex-lg-row gap-4 justify-content-between align-items-center border-bottom p-3">
           <ViewSelect handleViewSelect={handleViewSelect} />
-          <button className="btn-capital">Create Meeting</button>
+          <button className="btn-capital lh-1">Create Meeting</button>
         </div>
 
-        <div className="schedule__container px-3 d-flex justify-content-between gap-5">
-          <div className="calender__div w-75">
+        <div className="schedule__container px-3">
+          <div className="calender__div">
             <CalendarContainer view={view} />
           </div>
-          <div className="meetings__div w-25 p-3 border rounded-4">
-            <h1>My Meetings</h1>
+          <div className="meetings__div p-3 border rounded-4">
+            <details className="border">
+              <summary className="border border-danger">Daily Meetings</summary>
+              <ul>
+                <li>Meeting 1</li>
+                <li>Meeting 2</li>
+                <li>Meeting 3</li>
+              </ul>
+            </details>
           </div>
         </div>
       </section>
