9079897ac08b3054ecfd2d2d1c502f5a380bc1b2
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-10-06 18:15:51 +0530

    nike page 75% completed

---
+++ src/Images/Ecommerace/NikeLandingpage/index.js
@@ -0,0 +1,21 @@
+import nikelogo from './nikelogo.png'
+import whiteBlueOrange from './whiteBlueOrange.png'
+import blackOrangeRose from './blackOrangeRose.png'
+import blueYellowOrange from './blueYellowOrange.png'
+import star from './star.png'
+import groupOfPeople from './Group-of-people.png'
+import rosewhite from './rosewhite.png'
+import main from './Group 15438.png'
+
+
+
+export default{
+    nikelogo,
+    whiteBlueOrange,
+    blackOrangeRose,
+    blueYellowOrange,
+    star,
+    groupOfPeople,
+    rosewhite,
+    main
+}
