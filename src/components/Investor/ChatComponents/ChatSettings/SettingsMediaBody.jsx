3a0b58d8b99cd8083450003b6f903f20d6821ac9
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-10-09 12:11:54 +0530

    Minor Updates and bug fixes

--- src/components/Investor/ChatComponents/ChatSettings/SettingsMediaBody.jsx
+++ src/components/Investor/ChatComponents/ChatSettings/SettingsMediaBody.jsx
@@ -4,14 +4,16 @@ import { useSelector } from "react-redux";
 export default function SettingsMediaBody() {
   const chatProfile = useSelector((state) => state.chat.chatProfile);
   const communityProfile = useSelector((state) => state.chat.communityProfile);
-  const isCommunitySelected = useSelector((state) => state.chat.isCommunitySelected);
+  const isCommunitySelected = useSelector(
+    (state) => state.chat.isCommunitySelected
+  );
 
   return (
     <div className="px-2 py-3 d-flex gap-2 flex-wrap">
       {/* Loop media images here */}
       <div
-        className="bg-white rounded-2 d-flex justify-content-center align-items-center shadow-sm"
-        style={{ width: "100px", height: "100px" }}
+        className="bg-white rounded-2 d-flex justify-content-center align-items-center shadow-sm p-2"
+        style={{ height: "100px" }}
       >
         {isCommunitySelected
           ? communityProfile?.images?.map((image, index) => (
@@ -19,8 +21,8 @@ export default function SettingsMediaBody() {
                 key={index}
                 src={image.image}
                 alt="media item"
-                className="rounded-2"
-                style={{ width: "90px", height: "90px", marginRight: "10px" }}
+                className="rounded-2 object-fit-scale "
+                style={{ height: "90px" }}
               />
             ))
           : chatProfile?.images?.map((image, index) => (
@@ -28,8 +30,8 @@ export default function SettingsMediaBody() {
                 key={index}
                 src={image.image}
                 alt="media item"
-                className="rounded-2"
-                style={{ width: "90px", height: "90px" }}
+                className="rounded-2 object-fit-scale "
+                style={{ height: "90px" }}
               />
             ))}
       </div>
