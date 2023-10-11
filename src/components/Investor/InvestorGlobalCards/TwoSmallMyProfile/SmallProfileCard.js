b1ab2cc43e12ccf4429c450794cedbaa00d03dc3
srihari-m-s <srihari.magadi@gmail.com>
2023-10-11 12:56:28 +0530

    feat: Add respective tabs for explore page in startups and investors

--- src/components/Investor/InvestorGlobalCards/TwoSmallMyProfile/SmallProfileCard.js
+++ src/components/Investor/InvestorGlobalCards/TwoSmallMyProfile/SmallProfileCard.js
@@ -13,25 +13,27 @@ const SmallProfileCard = ({ className, text, width }) => {
   const formattedDate = formatDate(new Date().toDateString());
 
   return (
-    <div className={`container-fluid small_card_container ${className}`}>
-      {/* <div
-        className="d-flex flex-column flex-md-row align-items-center gap-1 gap-md-5 small_card_row"
-        style={{ width: width ? width : "100%" }}
-      >
-        <div className="">
-          <div className="box boxOne flex_content">
-            <h2 className="typography">Hello, {loggedInUser?.firstName}</h2>
-            <span className="smallest_typo">{formattedDate}</span>
+    <>
+      {/* <div className={`container-fluid small_card_container ${className}`}>
+        <div
+          className="d-flex flex-column flex-md-row align-items-center gap-1 gap-md-5 small_card_row"
+          style={{ width: width ? width : "100%" }}
+        >
+          <div className="">
+            <div className="box boxOne flex_content">
+              <h2 className="typography">Hello, {loggedInUser?.firstName}</h2>
+              <span className="smallest_typo">{formattedDate}</span>
+            </div>
           </div>
-        </div>
-        <div className="">
-          <div className="box boxTwo flex_content">
-            <img src={ArrowIcon} alt="arrow" />
-            <h2 className="typography">{text ? text : "My Profile"}</h2>
+          <div className="">
+            <div className="box boxTwo flex_content">
+              <img src={ArrowIcon} alt="arrow" />
+              <h2 className="typography">{text ? text : "My Profile"}</h2>
+            </div>
           </div>
         </div>
       </div> */}
-    </div>
+    </>
   );
 };
 
