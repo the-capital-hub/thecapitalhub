47bfc16f5dfa2ec99a02c4d2569a5b8793ffaeb4
srihari-m-s <srihari.magadi@gmail.com>
2023-10-16 17:32:40 +0530

    feat: Add onelink validation component

--- src/components/Investor/Support/Support.js
+++ src/components/Investor/Support/Support.js
@@ -2,17 +2,21 @@ import "./Support.scss";
 import ComingSoon from "../../ComingSoon/ComingSoon";
 import { useEffect } from "react";
 import MaxWidthWrapper from "../../Shared/MaxWidthWrapper/MaxWidthWrapper";
+import OnelinkValidation from "../../Shared/OnelinkValidation/OnelinkValidation";
 
 const Support = () => {
   useEffect(() => {
     document.title = "Support | The Capital Hub";
   }, []);
+
   return (
     <MaxWidthWrapper>
       <div className="support_container">
-        <div className="content-70 py-5">
+        <OnelinkValidation />
+
+        {/* <div className="content-70 py-5">
           <ComingSoon />
-        </div>
+        </div> */}
       </div>
     </MaxWidthWrapper>
   );
