539b9193323f17190360711b6a573f76745aa6fa
srihari-m-s <srihari.magadi@gmail.com>
2023-10-17 16:41:13 +0530

    fix: border left on xl and above screens

--- src/pages/InvestorOneLink/InvestorOneLinkInvestment/InvestorOneLinkPhilosophy.jsx
+++ src/pages/InvestorOneLink/InvestorOneLinkInvestment/InvestorOneLinkPhilosophy.jsx
@@ -10,8 +10,8 @@ export default function InvestorOneLinkInvestment() {
   let name, designation, location, image;
 
   return (
-    <div className="investment_philosophy_wrapper mb-5">
-      <h2>Investment Philosophy</h2>
+    <div className="investment_philosophy_wrapper mb-5 ps-3 leftBorder">
+      <h2 className="mb-3">Investment Philosophy</h2>
 
       {/* Main content */}
       <section className="investment_philosophy_section px-3 py-4 px-lg-4 bg-white rounded border d-flex flex-column gap-4">
@@ -32,7 +32,7 @@ export default function InvestorOneLinkInvestment() {
               {designation || "designation"}
             </p>
             <p className="fs-5 fw-light m-0 text-muted">
-              {location || "designation"}
+              {location || "location"}
             </p>
           </div>
         </header>
