b778ff773938fef30f4d0e90be8d97d3b25aac35
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-10-17 15:50:46 +0530

    Frontend Complete

--- src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
+++ src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import InvestedStartUpCard from "./InvestedStartUpCard/InvestedStartUpCard";
+import "./InvestorOneLinkStartups.scss";
 
 export default function InvestorOneLinkStartups() {
-  return <div>InvestorOneLinkStartups</div>;
+  const investedStartups = [1, 1];
+
+  return (
+    <section className="investor-onelink-startups-invested">
+      <h3>Startups Invested</h3>
+      <div className="startups-list">
+        {investedStartups?.map((startUp) => (
+          <InvestedStartUpCard startUp={startUp} />
+        ))}
+      </div>
+    </section>
+  );
 }
