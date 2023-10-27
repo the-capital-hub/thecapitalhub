07b52ad5fd0813840163583c3a745ceea1786585
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-10-27 10:45:39 +0530

    No media found text added

--- src/components/Investor/ChatComponents/ChatSettings/SettingsMediaBody.jsx
+++ src/components/Investor/ChatComponents/ChatSettings/SettingsMediaBody.jsx
@@ -8,33 +8,39 @@ export default function SettingsMediaBody() {
     (state) => state.chat.isCommunitySelected
   );
 
+  console.log(communityProfile?.images?.length || chatProfile?.images?.length);
+
   return (
     <div className="px-2 py-3 d-flex gap-2 flex-wrap">
       {/* Loop media images here */}
-      <div
-        className="bg-white rounded-2 d-flex justify-content-center align-items-center shadow-sm p-2"
-        style={{ height: "100px" }}
-      >
-        {isCommunitySelected
-          ? communityProfile?.images?.map((image, index) => (
-              <img
-                key={index}
-                src={image.image}
-                alt="media item"
-                className="rounded-2 object-fit-scale "
-                style={{ height: "90px" }}
-              />
-            ))
-          : chatProfile?.images?.map((image, index) => (
-              <img
-                key={index}
-                src={image.image}
-                alt="media item"
-                className="rounded-2 object-fit-scale "
-                style={{ height: "90px" }}
-              />
-            ))}
-      </div>
+      {(communityProfile?.images?.length || chatProfile?.images?.length) ? (
+        <div
+          className="bg-white rounded-2 d-flex justify-content-center align-items-center shadow-sm p-2"
+          style={{ height: "100px" }}
+        >
+          {isCommunitySelected
+            ? communityProfile?.images?.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.image}
+                  alt="media item"
+                  className="rounded-2 object-fit-scale "
+                  style={{ height: "90px" }}
+                />
+              ))
+            : chatProfile?.images?.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.image}
+                  alt="media item"
+                  className="rounded-2 object-fit-scale "
+                  style={{ height: "90px" }}
+                />
+              ))}
+        </div>
+      ) : (
+        <p className="text-center w-100 p-0 m-0">No media.</p>
+      )}
     </div>
   );
 }
