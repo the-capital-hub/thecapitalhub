9bbaf4995dc2882d7db6bf831bbd9fd6c1f6cbd1
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-10-30 17:14:31 +0530

    Show none options in filter

--- src/components/NewInvestor/FilterBySelect/FilterBySelect.jsx
+++ src/components/NewInvestor/FilterBySelect/FilterBySelect.jsx
@@ -4,9 +4,7 @@ function FilterBySelect({ name, label, onChange, options, value }) {
   return (
     <div className="filter_by_select">
       <select name={name} id={label} onChange={onChange} value={value}>
-        <option value="" hidden>
-          {label}
-        </option>
+        <option value={""}>{!value ? label : "None"}</option>
         {options?.map((option) => (
           <option key={option} value={option}>
             {option}
