fc2039e9faeb93fa13f62c29fa9a4fc60e4359a6
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-11-02 15:48:15 +0530

    Custom global styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./custom-bootstrap.scss";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
 import "./index.scss";
+import "./custom-bootstrap.scss";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./Store/Reducer/rootReducer";
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 import { store } from "./Store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
