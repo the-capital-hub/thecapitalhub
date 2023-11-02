9d0eac85803f50f56308c3a07ee26ab344286033
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-11-02 13:02:57 +0530

    Custom bootstrap integrated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "./custom-bootstrap.scss";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./index.scss";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
