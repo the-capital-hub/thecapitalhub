db9b31a629c4c6cdfc1f571e0275b4968ee1cb9b
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-11-06 15:53:19 +0530

    Links removed

--- src/components/ECommerace/Layout/Header/Header.jsx
+++ src/components/ECommerace/Layout/Header/Header.jsx
@@ -1,22 +1,25 @@
-import { Link, NavLink } from "react-router-dom";
+import React from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Header.scss";
 import Logo from "../../../../Images/Logo.svg";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className="container">
       <nav>
-        <div className="nav  justify-content-center align-items-center">
-          <NavLink to="/">
+        <div className="nav justify-content-center align-items-center">
+          {/* <NavLink to="/"> */}
+          <NavLink to={location.pathname}>
             <img src={Logo} alt="Logo" />
           </NavLink>
           {/* <NavLink to="/contactus " className='d-none d-md-flex'>Contact Us</NavLink> */}
           {/* <NavLink to="/about " className='d-none d-md-flex'>About Us</NavLink> */}
         </div>
       </nav>
-      {/* <Link to="/contactus" > */}
-      <Link to="/landing-page/food">
-        <button className="btn login-btn py-md-1">Contact us</button>
+      <Link to={location.pathname}>
+        <button className="btn login-btn px-3 py-md-1">Contact Us</button>
       </Link>
     </header>
   );
