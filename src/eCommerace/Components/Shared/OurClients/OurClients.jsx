567f170639fb715d6ac84e0e53bb27202ba359ec
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-11-06 10:43:53 +0530

    Design and content update

--- src/eCommerace/Components/Shared/OurClients/OurClients.jsx
+++ src/eCommerace/Components/Shared/OurClients/OurClients.jsx
@@ -3,14 +3,15 @@ import "./OurClients.scss";
 import { Link } from "react-router-dom";
 import assets from "../../../../Images/Portfolio/PortfolioPage";
 
-export default function OurClients() {
+export default function OurClients({ className }) {
   return (
-    <div className="our_clients_wrapper w-100">
+    <div className={`our_clients_wrapper w-100 ${className}`}>
       <div className="our_clients_container container mx-auto px-5 py-5">
         <div className="clients_heading">
           <h4>Our Clients</h4>
           <p>We do game-changing work for game-changing companies.</p>
-          <Link to={"/contactus"} className="text-capitalize work_button">
+          {/* <Link to={"/contactus"} className="text-capitalize work_button"> */}
+          <Link to={""} className="text-capitalize work_button">
             Work with us
           </Link>
         </div>
@@ -19,19 +20,19 @@ export default function OurClients() {
         <div className="client_logos d-flex flex-wrap gap-3 gap-lg-4">
           <img src={assets.yesgobus} alt="logo" width={100} height={80} />
           <img
-            src={assets.impactshall}
+            src={assets.impactshaala}
             alt="logo"
             width={200}
             height={"auto"}
             style={{ objectFit: "contain" }}
           />
-          <img
+          {/* <img
             src={assets.capitalHub}
             alt="logo"
             width={200}
             height={80}
             style={{ objectFit: "contain" }}
-          />
+          /> */}
           <img
             src={assets.circleIcon}
             alt="logo"
