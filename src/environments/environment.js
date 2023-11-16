a1b3f95f611a948bea37c6b599646b4e31fcca4e
Adithya <adithyahebbar32@gmail.com>
2023-11-16 18:57:12 +0530

    fixes

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // baseUrl:'https://thecapitalhub.onrender.com',
+  // baseUrl: "https://thecapitalhub.onrender.com",
   baseUrl: "http://localhost:8081",
   // baseUrl: "http://thc-env.eba-m5ah8qdm.ap-south-1.elasticbeanstalk.com",
 };
