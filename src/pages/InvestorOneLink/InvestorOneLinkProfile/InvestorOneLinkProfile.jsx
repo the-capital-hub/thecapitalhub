5668f75eaa6650cf845a35fbf1da98a00589a7a3
srihari-m-s <srihari.magadi@gmail.com>
2023-11-22 11:18:28 +0530

    fix: Fixes for removing left border, top margin of .content and alignment of page heading

--- src/pages/InvestorOneLink/InvestorOneLinkProfile/InvestorOneLinkProfile.jsx
+++ src/pages/InvestorOneLink/InvestorOneLinkProfile/InvestorOneLinkProfile.jsx
@@ -16,8 +16,10 @@ export default function InvestorOneLinkProfile() {
   console.log(company);
   return (
     <div className="investor_one_link_profile_page d-flex flex-column">
-      <h1>Investor Profile</h1>
-      <div className="main_inner-section d-flex flex-column gap-3">
+      <h1 className="mb-3 px-3 px-xxl-0 fw-bold page_heading">
+        Investor Profile
+      </h1>
+      <div className="main_inner-section d-flex flex-column gap-3 p-3">
         <div className="user_data_section d-flex flex-row gap-3 py-3">
           <img
             src={investor?.profilePicture}
--- src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
+++ src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
@@ -6,13 +6,13 @@ export default function InvestorOneLinkStartups() {
   const { company } = useOutletContext();
 
   return (
-    <section className="investor-onelink-startups-invested">
-      <h3>Startups Invested</h3>
+    <div className="investor-onelink-startups-invested">
+      <h3 className="px-3 px-xxl-0 fw-bold page_heading">Startups Invested</h3>
       <div className="startups-list">
         {company?.startupsInvested?.map((startUp) => (
           <InvestedStartUpCard startUp={startUp} />
         ))}
       </div>
-    </section>
+    </div>
   );
 }
