094689cbc952c0ad711dfcbd7550c29c42eac29a
srihari-m-s <srihari.magadi@gmail.com>
2023-11-24 18:52:11 +0530

    feat: Integrate Save, Save all functionality

--- src/pages/StartUp/Funding/FundingInfo.jsx
+++ src/pages/StartUp/Funding/FundingInfo.jsx
@@ -1,28 +1,76 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./FundingInfo.scss";
 import MaxWidthWrapper from "../../../components/Shared/MaxWidthWrapper/MaxWidthWrapper";
 import RightProfileCard from "../../../components/Investor/InvestorGlobalCards/RightProfileCard/RightProfileCard";
 import NewsCorner from "../../../components/Investor/InvestorGlobalCards/NewsCorner/NewsCorner";
 import RecommendationCard from "../../../components/Investor/InvestorGlobalCards/Recommendation/RecommendationCard";
 import { fundingQuestions } from "../../../constants/Startups/FundingInfo";
+import { useDispatch, useSelector } from "react-redux";
+import FundingFormFields from "../../../components/Investor/FundingPageComponents/FundingFormFields/FundingFormFields";
+import {
+  selectFundingQuestions,
+  selectLoggedInUserId,
+  setUserCompany,
+} from "../../../Store/features/user/userSlice";
+import SpinnerBS from "../../../components/Shared/Spinner/SpinnerBS";
+import { postStartUpData } from "../../../Service/user";
 
 export default function FundingInfo() {
-  const [editMode, setEditMode] = useState(false);
-  const [answers, setAnswers] = useState(null);
+  const loggedInUserId = useSelector(selectLoggedInUserId);
+  const fundingViaCapitalHubQuestions = useSelector(selectFundingQuestions);
 
-  useEffect(() => {
-    if (!answers) {
-      setEditMode(true);
-    }
-  }, [answers]);
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
 
   // Handle funding submit
   async function handleFundingSubmit(e) {
     e.preventDefault();
+    setLoading(true);
+
+    const {
+      targetMarket,
+      whyRightTimeForYourStartUp,
+      competitiveAdvantage,
+      biggestCompetitors,
+      revenueGenerated,
+    } = e.target;
+
+    let updatedData = {
+      fundingViaCapitalhubQuestions: {
+        targetMarket: targetMarket
+          ? targetMarket.value
+          : fundingViaCapitalHubQuestions["targetMarket"],
+        whyRightTimeForYourStartUp: whyRightTimeForYourStartUp
+          ? whyRightTimeForYourStartUp.value
+          : fundingViaCapitalHubQuestions["whyRightTimeForYourStartUp"],
+        competitiveAdvantage: competitiveAdvantage
+          ? competitiveAdvantage.value
+          : fundingViaCapitalHubQuestions["competitiveAdvantage"],
+        biggestCompetitors: biggestCompetitors
+          ? biggestCompetitors.value
+          : fundingViaCapitalHubQuestions["biggestCompetitors"],
+        revenueGenerated: revenueGenerated
+          ? revenueGenerated.value
+          : fundingViaCapitalHubQuestions["revenueGenerated"],
+      },
+      founderId: loggedInUserId,
+    };
+
+    try {
+      console.log("Save All", updatedData);
+      const { data } = await postStartUpData(updatedData);
+      console.log("answers:", data);
+      dispatch(setUserCompany(data));
+    } catch (error) {
+      console.error("Error Saving funding info:", error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
-    <div className="funding_wrapper my-4">
+    <div className="funding_wrapper my-4 mx-lg-3 mx-xl-0">
       <MaxWidthWrapper>
         <div className="two_col_wrapper">
           {/* Main content */}
@@ -36,27 +84,38 @@ export default function FundingInfo() {
               {/* Form */}
               <form
                 onSubmit={handleFundingSubmit}
-                className="px-3 px-lg-5 d-flex flex-column gap-3"
+                className="px-3 px-lg-5 d-flex flex-column gap-4"
               >
                 {Object.keys(fundingQuestions).map((question, index) => {
                   return (
-                    <fieldset key={question}>
-                      <legend className="fs-4">
-                        {fundingQuestions[question]}
-                      </legend>
-                      {editMode ? (
-                        <textarea
-                          name={question}
-                          id={`${index}${question}`}
-                          rows="5"
-                          className="funding_textarea"
-                        ></textarea>
-                      ) : (
-                        <p className="m-0"></p>
-                      )}
-                    </fieldset>
+                    <FundingFormFields
+                      question={question}
+                      key={question}
+                      answer={
+                        fundingViaCapitalHubQuestions
+                          ? fundingViaCapitalHubQuestions[question]
+                          : undefined
+                      }
+                    />
                   );
                 })}
+                <button
+                  type="submit"
+                  className="btn orange-button fs-6 d-flex align-items-center justify-content-center gap-2 w-50 mx-auto"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <>
+                      <SpinnerBS
+                        colorClass={"orange"}
+                        spinnerSizeClass="spinner-border-sm"
+                      />
+                      <span>Please wait...</span>
+                    </>
+                  ) : (
+                    "Save All"
+                  )}
+                </button>
               </form>
             </div>
           </div>
