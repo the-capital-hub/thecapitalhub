4a359c9f0c04f6b1bbc8363d2214165332032de4
srihari-m-s <srihari.magadi@gmail.com>
2023-12-04 15:06:56 +0530

    Add bootstrap css import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import "./custom-bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.bundle";
