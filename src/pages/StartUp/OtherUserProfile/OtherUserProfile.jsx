51c8ffa37819869d7331d5030dc5830fcca8878f
srihari-m-s <srihari.magadi@gmail.com>
2023-12-07 14:07:39 +0530

    style: Add dark mode colors to Manage account page

--- src/pages/StartUp/OtherUserProfile/OtherUserProfile.jsx
+++ src/pages/StartUp/OtherUserProfile/OtherUserProfile.jsx
@@ -64,7 +64,7 @@ function OtherUserProfile() {
   return (
     <div className="mt-5 mt-md-4">
       <MaxWidthWrapper>
-        <section className="other_user_profile">
+        <section className="other_user_profile mx-lg-4 mx-xl-0">
           <SmallProfileCard className="mt-lg-2 mt-xl-0" text="User Details" />
           {userData ? (
             <>
