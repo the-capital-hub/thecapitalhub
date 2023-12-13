fbbbb7633532de01d2c6aebfbd52aac6936adc5b
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-12-13 16:43:22 +0530

    profile info page added in settings

--- src/components/Investor/UserSettingsPageComponents/Account/Display/Display.jsx
+++ src/components/Investor/UserSettingsPageComponents/Account/Display/Display.jsx
@@ -7,7 +7,7 @@ export default function Display() {
     <div className="display-wrapper d-flex flex-column shadow-sm">
       <h4 className="m-0 px-3 py-3">Display</h4>
       <div className="border-top px-3 py-3">
-        <SettingsBar text={"Dark Mode"} />
+        <SettingsBar text={"Dark Mode"} link={"dark_mode_setting"} />
       </div>
     </div>
   );
