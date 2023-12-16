23c3308435e002b308dbae8dc5d3d41bc8787947
srihari-m-s <srihari.magadi@gmail.com>
2023-12-16 17:54:13 +0530

    feat: Add mobile view changes for calendar in Appointment page and update document titles in each page

--- src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
+++ src/pages/InvestorOneLink/InvestorOneLinkStartups/InvestorOneLinkStartups.jsx
@@ -1,9 +1,19 @@
 import { useOutletContext } from "react-router";
 import InvestedStartUpCard from "./InvestedStartUpCard/InvestedStartUpCard";
 import "./InvestorOneLinkStartups.scss";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setPageTitle } from "../../../Store/features/design/designSlice";
 
 export default function InvestorOneLinkStartups() {
   const { company } = useOutletContext();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    document.title = "Startups Invested | The Capital Hub";
+    dispatch(setPageTitle("Startups Invested"));
+  }, [dispatch]);
 
   return (
     <div className="investor-onelink-startups-invested">
