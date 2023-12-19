5a8ee7abfb5edc2b319a6f213ce36c600f535333
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-12-19 12:01:49 +0530

    speling changes in faq section

--- src/components/Investor/Help/components/Faq/question.js
+++ src/components/Investor/Help/components/Faq/question.js
@@ -1,14 +1,14 @@
 const questions = [
   {
-    q: "What is the 'One Like' feature on CapitalHub?",
-    a: "The 'One Like' feature on CapitalHub allows users to express their interest in a startup's profile with a single click.",
+    q: "What is the 'OneLink' feature on Capital Hub?",
+    a: "The 'OneLink' feature on Capital Hub allows users to express their interest in a startup's profile with a single click.",
   },
   {
-    q: "Is the 'One Like' feature visible to other users on CapitalHub?",
-    a: "No, the 'One Like' feature is a private action. It does not display your interest to other users, maintaining your privacy.",
+    q: "Is the 'OneLink' feature visible to other users on Capital Hub?",
+    a: "No, the 'OneLink' feature is a private action. It does not display your interest to other users, maintaining your privacy.",
   },
   {
-    q: "How does the 'Explore' feature on CapitalHub work?",
+    q: "How does the 'Explore' feature on Capital Hub work?",
     a: "The 'Explore' feature allows users to discover and filter startups, founders, and investors according to their preferences and criteria.",
   },
   {
@@ -24,15 +24,15 @@ const questions = [
     a: "The search results are typically updated in real-time, ensuring you have access to the most current profiles that match your filters.",
   },
   {
-    q: "Can I use the 'Explore' feature to network with other users on CapitalHub?",
+    q: "Can I use the 'Explore' feature to network with other users on Capital Hub?",
     a: "Yes, the 'Explore' feature not only helps you find opportunities but also facilitates networking and connections with like-minded users.",
   },
   {
-    q: "Can I upload documents to CapitalHub for my startup or investment profile?",
-    a: "Yes, CapitalHub allows users to upload relevant documents, such as business plans, pitch decks, financial statements, and more, to enhance their profiles.",
+    q: "Can I upload documents to Capital Hub for my startup or investment profile?",
+    a: "Yes, Capital Hub allows users to upload relevant documents, such as business plans, pitch decks, financial statements, and more, to enhance their profiles.",
   },
   {
-    q: "How do I upload documents to my CapitalHub profile?",
+    q: "How do I upload documents to my Capital Hub profile?",
     a: "To upload documents, access your profile, and look for the 'Document Upload' section. Follow the instructions to upload and manage your files.",
   },
   {
@@ -40,23 +40,23 @@ const questions = [
     a: "You can typically control the visibility of your documents. You can choose to share them with specific users or make them public on your profile.",
   },
   {
-    q: "How do I create a post on CapitalHub?",
-    a: "To create a post, log in to your CapitalHub account, navigate to the 'Create Post' section, and follow the provided prompts to compose and share your content.",
+    q: "How do I create a post on Capital Hub?",
+    a: "To create a post, log in to your Capital Hub account, navigate to the 'Create Post' section, and follow the provided prompts to compose and share your content.",
   },
   {
-    q: "What can I post on CapitalHub using the 'Create Post' feature?",
+    q: "What can I post on Capital Hub using the 'Create Post' feature?",
     a: "You can share a variety of content, including news, articles, updates about your startup, investment insights, industry trends, and more.",
   },
   {
     q: "What is the 'Create Community' feature, and how does it work?",
-    a: "'Create Community' allows users to establish their own dedicated community spaces within CapitalHub, tailored to specific interests or industries.",
+    a: "'Create Community' allows users to establish their own dedicated community spaces within Capital Hub, tailored to specific interests or industries.",
   },
   {
-    q: "How can I create a community on CapitalHub?",
+    q: "How can I create a community on Capital Hub?",
     a: "To create a community, go to the 'Create Community' section, provide the necessary details, and customize your community's settings and preferences.",
   },
   {
-    q: "Can I invite specific users to join my community, or is it open to all CapitalHub members?",
+    q: "Can I invite specific users to join my community, or is it open to all Capital Hub members?",
     a: "Depending on your preferences, you can make your community public and open to all members or send invitations to specific users to join.",
   },
   {
@@ -72,21 +72,21 @@ const questions = [
     a: "'My Community' often offers tools for member invites, content moderation, member roles, and other features to help you maintain a positive community environment.",
   },
   {
-    q: "How do I save a post on CapitalHub for future reference?",
+    q: "How do I save a post on Capital Hub for future reference?",
     a: "To save a post, you can typically click on a 'Save' or 'Bookmark' option associated with the post.",
   },
   {
-    q: "Are saved posts visible to other users on CapitalHub?",
+    q: "Are saved posts visible to other users on Capital Hub?",
     a: "Saved posts are generally private and visible only to you. They serve as a personal collection of content you wish to revisit.",
   },
 
   //   {
-  //     q: "How do I use the 'One Like' feature to express interest in a startup's profile?",
-  //     a: "To use 'One Like,' simply navigate to the startup's profile and click the 'Like' button. This action notifies the startup that you are interested in their project."
+  //     q: "How do I use the 'OneLink' feature to express interest in a startup's profile?",
+  //     a: "To use 'OneLink,' simply navigate to the startup's profile and click the 'Like' button. This action notifies the startup that you are interested in their project."
   //   },
   //   {
-  //     q: "What happens after I 'One Like' a startup's profile?",
-  //     a: "When you 'One Like' a startup, they receive a notification indicating your interest. This can potentially lead to further discussions and connections."
+  //     q: "What happens after I 'OneLink' a startup's profile?",
+  //     a: "When you 'OneLink' a startup, they receive a notification indicating your interest. This can potentially lead to further discussions and connections."
   //   },
   //   {
   //     q: "Can I save my search preferences in the 'Explore' section for future use?",
@@ -98,14 +98,14 @@ const questions = [
   //   },
   //   {
   //     q: "Are there any restrictions on the types of documents I can upload?",
-  //     a: "While CapitalHub generally accepts a wide range of document types, it's important to adhere to any guidelines or restrictions outlined on the platform."
+  //     a: "While Capital Hub generally accepts a wide range of document types, it's important to adhere to any guidelines or restrictions outlined on the platform."
   //   },
   //   {
-  //     q: "What is the 'Create Post' feature on CapitalHub?",
-  //     a: "The 'Create Post' feature allows users to share updates, insights, and information with the CapitalHub community through posts."
+  //     q: "What is the 'Create Post' feature on Capital Hub?",
+  //     a: "The 'Create Post' feature allows users to share updates, insights, and information with the Capital Hub community through posts."
   //   },
   //   {
-  //     q: "What are some use cases for creating a community on CapitalHub?",
+  //     q: "What are some use cases for creating a community on Capital Hub?",
   //     a: "You can create a community to discuss industry-specific topics, connect with like-minded individuals, or facilitate discussions around a particular niche."
   //   },
   //   {
@@ -113,7 +113,7 @@ const questions = [
   //     a: "'My Community' is a dashboard that allows community administrators to oversee and customize their community, manage members, and moderate content."
   //   },
   //   {
-  //     q: "What is the 'Saved Posts' feature on CapitalHub?",
+  //     q: "What is the 'Saved Posts' feature on Capital Hub?",
   //     a: "The 'Saved Posts' feature allows users to bookmark and organize posts that they find interesting and want to revisit later."
   //   },
   //   {
