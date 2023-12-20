b9bb3d10c514fca1d2610895f1de92d86ee71cc5
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-12-20 11:56:57 +0530

    8. Rename "Looking for funding?" to "Apply for funding"

--- src/components/Investor/Feed/Components/LookingForFund/LookingForFund.jsx
+++ src/components/Investor/Feed/Components/LookingForFund/LookingForFund.jsx
@@ -6,7 +6,7 @@ export default function LookingForFund() {
   return (
     <div className="looking_for_fund_wrapper bg-black text-white py-3">
       <div className="d-flex align-items-center justify-content-around justify-content-md-between px-md-4 flex-wrap gap-3">
-        <p className="m-0 fs-5">Looking for Funding?</p>
+        <p className="m-0 fs-5">Apply for funding.</p>
 
         <Link
           to={"/funding"}
