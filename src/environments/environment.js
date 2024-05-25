9001618147eb7b7c77d852d55f9786618afa73cc
The Capital Hub <141011134+the-capital-hub@users.noreply.github.com>
2024-05-25 12:44:13 +0530

    Update environment.js

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // baseUrl: "https://thecapitalhub.onrender.com",
-  baseUrl: "http://localhost:8080",
+  baseUrl: "https://thecapitalhub.onrender.com",
+  //baseUrl: "http://localhost:8080",
   //baseUrl: "https://api.thecapitalhub.in",
 };
